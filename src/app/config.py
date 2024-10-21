93fd3ca6fb8b86a312f232d9520efedf1c3bc5c6
Fernando Abreu <49952773+nandoabreu@users.noreply.github.com>
2024-10-21 21:00:16 +0100

    Update env var and env var config (parser)

---
+++ src/app/config.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""Project configuration file.
+
+*** Update `env.toml` and run `make` to set `.env`. ***
+
+This module manages environment variables and fallback values:
+- Modify only to add/remove vars or to update fallback values.
+- Use the Makefile to keep `.env` updated (values in `.env` are not persistent).
+
+*** `.env` should be deployed to production. ***
+
+Regarding decouple, the dependency responsible for fetching environment variables, the precedence order is:
+- command-line variable > .env file > fallback value (if set)
+"""
+from decouple import config
+from pathlib import Path
+from tempfile import gettempdir
+
+LOG_LEVEL: str = config('LOG_LEVEL', default='INFO')
+
+APP_NAME: str = config('APP_NAME', default='App Name')
+APP_VERSION: str = config('APP_VERSION', default='0.1.0')
+PROJECT_NAME: str = config('PROJECT_NAME', default=APP_NAME).replace(' ', '-').lower()
+PROJECT_DESCRIPTION: str = config('PROJECT_DESCRIPTION', default=APP_NAME)
+
+LOG_ROTATION_MAX_MB: float = config('LOG_ROTATION_MAX_MB', cast=float, default='9')
+LOG_MAX_ROTATED_FILES: int = config('LOG_MAX_ROTATED_FILES', cast=int, default='5')
+
+LOGS_DIR: Path = Path(config('LOGS_DIR', default=f'/{gettempdir()}/{PROJECT_NAME}')).resolve()
+APP_DIR: Path = Path(config('APP_DIR', default='app')).resolve()
+SRC_BASE_DIR: Path = Path(__file__).resolve().parent.parent
+PROJECT_ROOT_DIR: Path = SRC_BASE_DIR.parent
+
+SOURCE_XML_URL: str = config('SOURCE_XML_URL')
