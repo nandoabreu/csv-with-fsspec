3a3d52fce24e117eec82331d1b70abaa4d596a5d
Fernando Abreu <49952773+nandoabreu@users.noreply.github.com>
2024-10-22 17:55:44 +0100

    Minor log replacement

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -26,18 +26,18 @@ def main():
         rotation_mb=LOG_ROTATION_MAX_MB,
     )
     log = obj.logger
-
     log.info('{s}- Start {a} v{v} {s}-'.format(s='-*' * 5, a=APP_NAME, v=APP_VERSION))
 
     extractor = Extractor()
 
     try:
         package_url = extractor.fetch_package_url(source_xml_url=SOURCE_XML_URL, link_index=DOWNLOAD_LINK_INDEX)
-        log.debug(f'Fetched: {package_url=}')
 
     except (ConnectionError, HTTPError) as e:
         log.error('Could not fetch file - Update the SOURCE_XML_URL var in env.toml and/or .env. See logs for details.')
         log.debug(e)
+
+    log.debug(f'Parse data from {package_url=}')
 
 
 if __name__ == '__main__':
