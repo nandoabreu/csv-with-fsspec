2a5021422700c7f4ecae9318b4fd38705462d235
Fernando Abreu <49952773+nandoabreu@users.noreply.github.com>
2024-10-22 21:32:28 +0100

    Init Transformer class

---
+++ src/app/Transformer.py
@@ -0,0 +1,9 @@
+"""Transformer module
+
+This module handles transformation of data
+"""
+
+
+class Transformer:
+    def __init__(self):
+        ...
---
+++ tests/unit/app-Transformer-test.py
@@ -0,0 +1,3 @@
+import pytest
+
+from app.Transformer import Transformer
